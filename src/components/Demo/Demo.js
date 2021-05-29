6598e9d1c9b99e0a9522b1481955e9697ff3d12f
Sajid Chowdhury <msrchk@gmail.com>
2021-05-30 01:37:20 +0600

    added

---
+++ src/components/Demo/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laborum doloribus iusto labore explicabo in adipisci ex provident natus voluptatibus rem illum hic quis expedita beatae facilis animi unde assumenda error, libero sit earum? Magni dicta a suscipit minus odit obcaecati ratione? Unde, voluptas debitis!</p>
+        </div>
+    );
+};
+
+export default Demo;
