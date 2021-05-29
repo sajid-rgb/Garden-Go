40421cb664193a0eb40cf9bffeb6b380d0b4dd2d
Sajid Chowdhury <msrchk@gmail.com>
2021-05-30 01:27:06 +0600

    Private route start

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivateRoute = () => {
+    return (
+        <div>
+            <p>Hello I am PrivateRoute.......</p>
+        </div>
+    );
+};
+
+export default PrivateRoute;
